6cb033d2880711d79de341a3cdacaf3e37308212
jedidiah <jedidahfavour@gmail.com>
2024-01-19 19:58:22 +0300

    fix puppet-lint errors and add killmenow process file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,10 +2,10 @@
 
 # Create a file in the directory /tmp
 file { '/tmp/school':
-  ensure     => present,
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 
